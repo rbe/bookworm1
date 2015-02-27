dc8ac9061c52cf561bbb97f6e453b2e117ce2e13
Ralf Bensmann <ralf@art-of-coding.eu>
2015-02-27 09:34:40 +0100

    Added bookworm.assembly module

---
+++ eu.artofcoding.bookworm.assembly/src/test/java/eu/artofcoding/bookworm/assembly/ContainerITSuite.java
@@ -0,0 +1,18 @@
+/*
+ * Bookworm
+ *
+ * Copyright (C) 2011-2015 art of coding UG, http://www.art-of-coding.eu
+ * Alle Rechte vorbehalten. Nutzung unterliegt Lizenzbedingungen.
+ * All rights reserved. Use is subject to license terms.
+ */
+
+package eu.artofcoding.bookworm.assembly;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({WaitForContainerIT.class})
+public class ContainerITSuite {
+}
---
+++ eu.artofcoding.bookworm.assembly/src/test/java/eu/artofcoding/bookworm/assembly/WaitForContainerIT.java
@@ -0,0 +1,27 @@
+/*
+ * Bookworm
+ *
+ * Copyright (C) 2011-2015 art of coding UG, http://www.art-of-coding.eu
+ * Alle Rechte vorbehalten. Nutzung unterliegt Lizenzbedingungen.
+ * All rights reserved. Use is subject to license terms.
+ */
+
+package eu.artofcoding.bookworm.assembly;
+
+import org.junit.Test;
+
+public class WaitForContainerIT {
+
+    private static final int WAIT = 20;
+
+    @Test
+    public void shouldStartContainer() {
+        System.out.printf("Waiting %d seconds for container to startup%n", WAIT);
+        try {
+            Thread.sleep(WAIT * 1000);
+        } catch (InterruptedException e) {
+            System.out.printf("Interrupted waiting for container to startup: %s%n", e.getMessage());
+        }
+    }
+
+}
