27f2ec770ab89229ad0cab9f3725364861843db1
Ralf Bensmann <ralf@art-of-coding.eu>
2015-03-21 13:04:19 +0100

    Resolve templates against path set through environment variable

--- eu.artofcoding.bookworm.customer/web/src/main/java/eu/artofcoding/bookworm/customer/web/jsf/FaceletsResourceResolver.java
+++ eu.artofcoding.bookworm.customer/web/src/main/java/eu/artofcoding/bookworm/customer/web/jsf/FaceletsResourceResolver.java
@@ -12,10 +12,24 @@ import javax.faces.view.facelets.ResourceResolver;
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.logging.Logger;
 
 public class FaceletsResourceResolver extends ResourceResolver {
 
+    private static final Logger LOGGER = Logger.getLogger(FaceletsResourceResolver.class.toString());
+
+    private static final String TEMPLATE_SYSTEM_VARIABLE = "WBH_CUSTOMER_TEMPLATE";
+
+    private static final String BASE_PATH;
+
     private ResourceResolver parent;
+
+    static {
+        BASE_PATH = System.getenv(TEMPLATE_SYSTEM_VARIABLE);
+        if (null == BASE_PATH) {
+            LOGGER.warning(String.format("Variable %s not set!", TEMPLATE_SYSTEM_VARIABLE));
+        }
+    }
 
     public FaceletsResourceResolver(final ResourceResolver parent) {
         this.parent = parent;
@@ -24,10 +38,9 @@ public class FaceletsResourceResolver extends ResourceResolver {
     @Override
     public URL resolveUrl(final String path) {
         URL url = parent.resolveUrl(path);
-        if (url == null) {
+        if (url == null && null != BASE_PATH) {
             try {
-                final String basePath = System.getenv("WBH_CUSTOMER_TEMPLATE");
-                url = new File(basePath, path).toURI().toURL();
+                url = new File(BASE_PATH, path).toURI().toURL();
             } catch (MalformedURLException e) {
                 throw new RuntimeException(e);
             }
