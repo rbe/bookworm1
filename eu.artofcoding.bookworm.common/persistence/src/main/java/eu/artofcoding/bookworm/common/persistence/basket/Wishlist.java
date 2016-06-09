560687b9595f16769eb41688284a540917d8c95a
Ralf Bensmann <ralf@art-of-coding.eu>
2016-06-09 08:40:24 +0200

    Wishlist

---
+++ eu.artofcoding.bookworm.common/persistence/src/main/java/eu/artofcoding/bookworm/common/persistence/basket/Wishlist.java
@@ -0,0 +1,94 @@
+/*
+ * Bookworm
+ *
+ * Copyright (C) 2011-2015 art of coding UG, http://www.art-of-coding.eu
+ * Alle Rechte vorbehalten. Nutzung unterliegt Lizenzbedingungen.
+ * All rights reserved. Use is subject to license terms.
+ */
+
+package eu.artofcoding.bookworm.common.persistence.basket;
+
+import eu.artofcoding.beetlejuice.api.persistence.GenericEntity;
+import eu.artofcoding.bookworm.common.persistence.book.Book;
+import org.apache.openjpa.persistence.jdbc.ForeignKey;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Version;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@NamedQueries({
+        @NamedQuery(name = "Wishlist.findByHoerernummer", query = "SELECT o FROM Wishlist o WHERE o.hoerernummer = :hoerernummer")
+})
+public class Wishlist implements GenericEntity, Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Version
+    private Long version;
+
+    @Basic
+    @Column(nullable = false)
+    @Size(min = 1, max = 5)
+    private String hoerernummer;
+
+    @OneToMany(fetch = FetchType.EAGER)
+    @ForeignKey(enabled = false)
+    private List<Book> books = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getVersion() {
+        return version;
+    }
+
+    public void setVersion(Long version) {
+        this.version = version;
+    }
+
+    public String getHoerernummer() {
+        return hoerernummer;
+    }
+
+    public void setHoerernummer(final String hoerernummer) {
+        this.hoerernummer = hoerernummer;
+    }
+
+    public List<Book> getBooks() {
+        if (null == books) {
+            books = new ArrayList<>();
+        }
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+
+    public int itemCount() {
+        return getBooks().size();
+    }
+
+}
