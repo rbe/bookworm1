9116a51f3ba76833d899a01849877ba7204f7ab7
Ralf Bensmann <ralf@art-of-coding.eu>
2016-06-30 12:16:19 +0200

    Regular export of BlistaOrder data

--- src/main/resources/webhoer.sql
+++ src/main/resources/webhoer.sql
@@ -1,29 +1,32 @@
 CREATE OR REPLACE VIEW v_webhoer AS
   SELECT
-    bo.hoerernummer HOENR
+      bo.hoerernummer HOENR
     , bob.books_titelnummer TITNR
     , b.aghnummer TIAGNR
-    , 12345678901 DLSID -- TODO bo.dlsid
+    , 'unbekannt01' DLSID -- TODO bo.dlsid
     , DATE_FORMAT(NOW(), '%Y%m%d') ABFRDT
     , DATE_FORMAT(NOW(), '%H%i%s') ABFRZT
-    , 3 STATUS -- TODO
+    , 0 STATUS -- TODO
     , DATE_FORMAT(bo.ausleihdatum, '%Y%m%d') AUSLDT
     , DATE_FORMAT(bo.ausleihdatum, '%H%i%s') AUSLZT
-    , DATE_FORMAT(bo.ausleihdatum, '%Y%m%d') RUEGDT -- TODO
-    , DATE_FORMAT(bo.ausleihdatum, '%H%i%s') RUEGZT -- TODO
+    , DATE_FORMAT(bo.ausleihdatum + INTERVAL 1 MONTH, '%Y%m%d') RUEGDT -- TODO
+    , DATE_FORMAT(bo.ausleihdatum + INTERVAL 1 MONTH, '%H%i%s') RUEGZT -- TODO
   FROM BlistaOrder bo
     INNER JOIN BlistaOrder_Book bob ON bo.id = bob.blistaorder_id
     INNER JOIN Book b ON bob.books_titelnummer = b.titelnummer
     INNER JOIN HoererBuchstamm hbs ON hbs.hoerernummer = bo.hoerernummer
 ;
 
+
+DROP EVENT webhoer_csv;
+
 DELIMITER $$
 
 CREATE EVENT webhoer_csv
-  ON SCHEDULE EVERY 1 DAY
+  ON SCHEDULE EVERY 18 DAY_HOUR
 DO BEGIN
-  SELECT
-    CONCAT(
+  SET @filepath = CONCAT('/home/wbh/apache/sites/wbh-online.de/www/app/download/webhoer-', DATE_FORMAT(NOW(), '%Y%m%d'), '.csv');
+  SET @sql = CONCAT("SELECT CONCAT(
         LPAD(hoenr, 5, ' ')
         , LPAD(titnr, 6, ' ')
         , LPAD(tiagnr, 13, ' ')
@@ -37,13 +40,10 @@ DO BEGIN
         , LPAD(ruegzt, 6, ' ')
     )
   FROM v_webhoer
-  INTO OUTFILE '/tmp/webhoer.csv'
-    -- FIELDS ENCLOSED BY ''
-    -- FIELDS TERMINATED BY ''
-    -- ESCAPED BY ''
-  LINES TERMINATED BY '\r\n'
-  ;
-
+  INTO OUTFILE '", @filepath, "' LINES TERMINATED BY '\r\n'");
+  PREPARE stmt FROM @sql;
+  EXECUTE stmt;
+  DEALLOCATE PREPARE stmt;
 END $$
 
 DELIMITER ;
