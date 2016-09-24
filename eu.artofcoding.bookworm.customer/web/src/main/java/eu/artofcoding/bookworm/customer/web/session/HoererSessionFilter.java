24b83d60abcef26e715f45195672a0a9a668ab4a
Ralf Bensmann <ralf@art-of-coding.eu>
2016-09-24 15:18:09 +0200

    customer.web logout 

---
+++ eu.artofcoding.bookworm.customer/web/src/main/java/eu/artofcoding/bookworm/customer/web/session/HoererSessionFilter.java
@@ -0,0 +1,45 @@
+package eu.artofcoding.bookworm.customer.web.session;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter(filterName = "HoererSessionFilter", urlPatterns = "/*")
+public class HoererSessionFilter implements Filter {
+
+    private static final String HNR_KEY = "hnr";
+
+    @Override
+    public void destroy() {
+        /* Do nothing */
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+        final HttpServletRequest request = (HttpServletRequest) req;
+        final HttpSession session = ((HttpServletRequest) req).getSession();
+        if (request.getRequestURI().endsWith("logout")) {
+            session.invalidate();
+        } else {
+            final Object sessionHnr = session.getAttribute(HNR_KEY);
+            if (null == sessionHnr) {
+                final String hnr = req.getParameter(HNR_KEY);
+                session.setAttribute(HNR_KEY, hnr);
+            }
+            chain.doFilter(req, resp);
+        }
+    }
+
+    @Override
+    public void init(FilterConfig config) throws ServletException {
+        /* Do nothing */
+    }
+
+}
