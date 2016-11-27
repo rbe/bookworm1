19f18abfd05faeb8385bdacdd67210ab6b504cb4
Ralf Bensmann <ralf@art-of-coding.eu>
2016-11-27 11:46:59 +0100

    Added HoererSessionFilter (after EncodingFilter)

--- eu.artofcoding.bookworm.catalog/web/src/main/java/eu/artofcoding/bookworm/catalog/web/session/HoererSessionFilter.java
+++ eu.artofcoding.bookworm.catalog/web/src/main/java/eu/artofcoding/bookworm/catalog/web/session/HoererSessionFilter.java
@@ -6,12 +6,11 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebFilter(filterName = "HoererSessionFilter", urlPatterns = "/*")
+// See web.xml @WebFilter(filterName = "HoererSessionFilter", urlPatterns = "/*")
 public class HoererSessionFilter implements Filter {
 
     private static final String HNR_KEY = "hnr";
@@ -22,20 +21,22 @@ public class HoererSessionFilter implements Filter {
     }
 
     @Override
-    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+    public void doFilter(final ServletRequest req, final ServletResponse resp, final FilterChain chain) throws ServletException, IOException {
         final HttpServletRequest request = (HttpServletRequest) req;
-        final HttpSession session = ((HttpServletRequest) req).getSession();
-        if (request.getRequestURI().endsWith("logout")) {
-            session.invalidate();
-        } else {
-            final String sessionHnr = (String) session.getAttribute(HNR_KEY);
-            final boolean sessionHasNoHnr = null == sessionHnr || sessionHnr.isEmpty();
-            if (sessionHasNoHnr) {
-                final String hnr = req.getParameter(HNR_KEY);
-                session.setAttribute(HNR_KEY, hnr);
+        final HttpSession session = ((HttpServletRequest) req).getSession(true);
+        if (null != session) {
+            if (request.getRequestURI().endsWith("logout")) {
+                session.invalidate();
+            } else {
+                final String sessionHnr = (String) session.getAttribute(HNR_KEY);
+                final boolean sessionHasNoHnr = null == sessionHnr || sessionHnr.isEmpty();
+                if (sessionHasNoHnr) {
+                    final String hnr = req.getParameter(HNR_KEY);
+                    session.setAttribute(HNR_KEY, hnr);
+                }
             }
-            chain.doFilter(req, resp);
         }
+        chain.doFilter(req, resp);
     }
 
     @Override
